3b0a72160f6b51df162c1befbfcd8a5a4b089715
syihwamoza <mozakastella26@gmail.com>
2025-11-27 20:37:34 +0700

    Menambahkan DestinasiHome sebagai rute halaman utama

---
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiHome.kt
@@ -0,0 +1,7 @@
+package com.example.pertemuan9.view.route
+import com.example.pertemuan9.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.entry_siswa
+}
