a30cd72b6849cfc44a6736aed3ef200c3f4e983a
syihwamoza <mozakastella26@gmail.com>
2025-11-27 20:48:23 +0700

    Menambahkan alur StateFlow homeUiState pada HomeViewModel

--- app/src/main/java/com/example/pertemuan9/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/HomeViewModel.kt
@@ -15,3 +15,13 @@ class HomeViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+
+        )
+
