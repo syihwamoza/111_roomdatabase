a437fd1ba3492e716feac6c76734e6f6ccf8f110
syihwamoza <mozakastella26@gmail.com>
2025-11-27 20:43:03 +0700

    Menambahkan EntryViewModel dengan validasi input dan state siswa

--- app/src/main/java/com/example/pertemuan9/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/EntryViewModel.kt
@@ -1,41 +1,21 @@
-package com.example.pertemuan9.viewmodel
+package com.example.Pertemuan9.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.pertemuan9.repositori.RepositoriSiswa
 import com.example.pertemuan9.room.Siswa
 
-class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
-    var uiStateSiswa by mutableStateOf(UIstateSiswa())
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+    // Berisi status Siswa saat ini
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
-    private fun validasiInput (uistate: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean{
-        return with(uistate){
-            name.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+
+    // Fungsi untuk memvalidasi input
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
-
-data class DetailSiswa(
-    val id: Int = 0,
-    val name: String = "",
-    val alamat: String = "",
-    val telpon: String = "",
-)
-
-fun DetailSiswa.toSiswa(): Siswa = Siswa(
-    id = id,
-    name = name,
-    alamat = alamat,
-    telpon = telpon,
-)
-
-fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false ): UIStateSiswa = UIStateSiswa(
-    detailSiswa = this.toDetailSiswa(),
-    isEntryValid = isEntryValid
-)
-
-fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
-    id = id,
-    name = name,
-    alamat = alamat,
-    telpon = telpon
-)
