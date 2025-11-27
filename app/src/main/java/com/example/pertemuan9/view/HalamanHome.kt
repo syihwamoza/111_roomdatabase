56ed416502cb5a453478e7e841f8456bab8b710a
syihwamoza <mozakastella26@gmail.com>
2025-11-27 20:55:28 +0700

    Menambahkan komponen DataSiswa untuk menampilkan detail siswa dalam Card

--- app/src/main/java/com/example/pertemuan9/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan9/view/HalamanHome.kt
@@ -120,5 +120,39 @@ fun ListSiswa(
 
     }
 }
+@Composable
+fun DataSiswa(
+    siswa: Siswa,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small)))
+        {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(siswa.name,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(siswa.telpon,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(siswa.alamat,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+
+    }
+}
 
 
