3af84022a99b046456565e7bdc4cbddb643d7602
syihwamoza <mozakastella26@gmail.com>
2025-12-04 21:41:57 +0700

    Aktifkan navigasi ke Item Edit Siswa dengan ID

--- app/src/main/java/com/example/pertemuan9/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiEditSiswa.kt
@@ -5,6 +5,9 @@ import com.example.pertemuan9.R
 
 
 object DestinasiEditSiswa: DestinasiNavigasi {
-
+    override val route = "item_edit"
+    override val titleRes =  R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
 
